574def1e3aafa0cd0e93d4485dc6dcdf8c95f544
Laxit Rana <laxyrajput@gmail.com>
2023-05-30 06:28:12 +0530

    Made full frontend only styling is left

--- FrontEnd/dsatemplates/src/App.js
+++ FrontEnd/dsatemplates/src/App.js
@@ -1,29 +1,46 @@
-import React, { useState } from 'react';
-import Subjects from './Subjects';
-import Topics from './Topics';
-
-import './App.css';
+import { useState } from "react";
+import Topic from "./components/Topic";
+import SubTopic from "./components/SubTopic";
+import Question from "./components/Question";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState(null);
+  const [topic, setTopic] = useState(null);
+  const [subTopic, setSubTopic] = useState(null);
 
-  const handleSelectTopic = (topic) => {
-    setSelectedTopic(topic);
+  const onSelectTopic = (selectedTopic) => {
+    setTopic(selectedTopic);
+  };
+  const onSelectSubTopic = (selectedSubTopic) => {
+    setSubTopic(selectedSubTopic);
   };
 
-  let content;
-  if (selectedTopic) {
-    content = <Topics topic={selectedTopic} />;
+  if (!topic) {
+    return (
+      <div className="App">
+        <h1>Welcome to dsaTemplates</h1>
+        <Topic onSelectTopic={onSelectTopic} />
+      </div>
+    );
+  } else if (!subTopic) {
+    return (
+      <div className="App">
+        <h1>{topic}</h1>
+        <SubTopic
+          topic={topic}
+          onSelectSubTopic={onSelectSubTopic}
+        />
+      </div>
+    );
   } else {
-    content = <Subjects onSelectTopic={handleSelectTopic} />;
+    return (
+      <div className="App">
+        <h1>{subTopic}</h1>
+        <Question 
+        topic={topic}
+        subTopic={subTopic}/>
+      </div>
+    );
   }
-
-  return (
-    <div className="App">
-      <h1>Welcome to dsaTemplates</h1>
-      {content}
-    </div>
-  );
 }
 
 export default App;
---
+++ FrontEnd/dsatemplates/src/components/Question.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import "./Question.css";
+import leetcodeLogo from "./leetcode-logo.png";
+
+const Question = ({ topic, subTopic }) => {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/topics/${topic}/${subTopic}`);
+        setQuestions(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchQuestions();
+  }, [topic, subTopic]);
+
+  const getQuestionLinkElement = (question) => {
+    if (question.questionLink.includes("leetcode")) {
+      return (
+        <a href={question.questionLink}>
+          <img src={leetcodeLogo} alt="LeetCode" className="leetcode-logo" />
+        </a>
+      );
+    } else {
+      return <a href={question.questionLink}>Question Link</a>;
+    }
+  };
+
+  return (
+    <div className="Questions">
+      <div className="table-container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Question Link</th>
+              <th>Difficulty</th>
+              <th>Solution Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            {questions.map((question, index) => (
+              <tr key={index}>
+                <td>{question.title}</td>
+                <td>{getQuestionLinkElement(question)}</td>
+                <td>{question.difficulty}</td>
+                <td><a href={question.solutionLink}>Solution Link</a></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Question;
